04db6663417e76f57ce3865f756182bc6ca4c244
Travis Johnson <cjohnso0@mymail.mines.edu>
2015-04-18 11:46:21 -0600

    Random Noun Generator Working. Need to add longer lists of words.

--- app/src/main/java/mines_acmx/badlibs/Badlib.java
+++ app/src/main/java/mines_acmx/badlibs/Badlib.java
@@ -1,24 +1,62 @@
 package mines_acmx.badlibs;
 
+import android.content.DialogInterface;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import android.widget.Button;
+import android.view.View.OnClickListener;
+import android.view.View;
+import android.widget.TextView;
 
+import java.util.Arrays;
+import java.util.Random;
+import java.util.ArrayList;
+
+// The Main Activity for Badlibs project
 public class Badlib extends ActionBarActivity {
+
+    private ArrayList<String> noun;
+    private ArrayList<String> name;
+    private ArrayList<String> adjective;
+    private ArrayList<String> verb;
+    private ArrayList<String> adverb;
+    private static Random rand = new Random();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        initiateLists();
         setContentView(R.layout.activity_badlib);
     }
 
+    public void initiateLists(){
+        noun = new ArrayList<String>(Arrays.asList("turtle", "hat", "pig", "Mason"));
+
+    }
+
+    public String generateBadlib() {
+        return noun.get(rand.nextInt(noun.size()));
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_badlib, menu);
+
+        Button button = (Button) findViewById(R.id.generate);
+        button.setOnClickListener(new OnClickListener() {
+           @Override
+            public void onClick(View view) {
+               TextView textView = (TextView) findViewById(R.id.textView);
+               String badlib = generateBadlib();
+               textView.setText(badlib);
+           }
+        });
+
         return true;
     }
 
